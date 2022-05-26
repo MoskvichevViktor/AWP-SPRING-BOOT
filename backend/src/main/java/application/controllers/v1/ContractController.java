34cc11ce97003b730b42dc75b9f55b240f23c726
OldBrain <hel_brain@mail.ru>
2022-05-26 19:23:22 +0400

    Изменение контроллера согласно: https://docs.microsoft.com/ru-ru/azure/architecture/best-practices/api-design

--- backend/src/main/java/application/controllers/v1/ContractController.java
+++ backend/src/main/java/application/controllers/v1/ContractController.java
@@ -4,19 +4,20 @@ import application.exception.ResourceNotFoundException;
 import application.models.Contract;
 import application.services.ContractService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/contracts")
 @AllArgsConstructor
-
 public class ContractController {
     private final ContractService contractService;
 
-
-    @GetMapping("/all")
+    @GetMapping("")
     public List<Contract> getAllClients() {
         return contractService.findAll();
     }
