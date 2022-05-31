2d0a3ead13cdf235f58c98d3609f397913b6c7af
OldBrain <hel_brain@mail.ru>
2022-05-31 20:09:19 +0400

    Изменения в RequestService

--- backend/src/main/java/application/services/RequestService.java
+++ backend/src/main/java/application/services/RequestService.java
@@ -7,7 +7,7 @@ import application.exception.ResourceNotFoundException;
 import application.models.Client;
 import application.models.CreditRequest;
 import application.repositories.ClientRepository;
-import application.repositories.CreditRequestRepositoriy;
+import application.repositories.CreditRequestRepository;
 import application.repositories.CreditResponseRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -20,50 +20,45 @@ import java.util.stream.Collectors;
 @Service
 @RequiredArgsConstructor
 public class RequestService {
-    private final CreditRequestRepositoriy requestRepositoriy;
+    private final CreditRequestRepository requestRepository;
     private final ClientRepository clientRepository;
     private final CreditResponseRepository responseRepository;
 
+
     public List<CreditRequestDto> findAllRequestDto(RequestStatus status) {
-        return requestRepositoriy.findAll().stream()
+        return requestRepository.findAll().stream()
                 .filter(creditRequest -> status == null || creditRequest.getStatus().equals(status))
                 .map(CreditRequestDto::valueOf)
                 .collect(Collectors.toUnmodifiableList());
     }
 
-    public Optional<CreditRequestDto> findRequestDtoById(Long id){
-        return  requestRepositoriy.findById(id).map(CreditRequestDto::valueOf);
+    public Optional<CreditRequestDto> findRequestDtoById(Long id) {
+        return requestRepository.findById(id).map(CreditRequestDto::valueOf);
     }
 
-    public void save(CreditRequestInputDto requestInputDto){
-        if (clientRepository.existsById(requestInputDto.getClientId())){
+    public void save(CreditRequestInputDto requestInputDto) {
+        if (clientRepository.existsById(requestInputDto.getClientId())) {
             Client client = clientRepository.getById(requestInputDto.getClientId());
             CreditRequest newRequest = new CreditRequest();
             newRequest.setClient(client);
             newRequest.setSum(requestInputDto.getSum());
             newRequest.setPeriod(requestInputDto.getPeriod());
             newRequest.setStatus(RequestStatus.WAITING);
-            requestRepositoriy.save(newRequest);
+            requestRepository.save(newRequest);
         }
     }
 
     public void update(CreditRequestDto requestDto) {
 
-
-        if (requestRepositoriy.existsById(requestDto.getId())) {
-            CreditRequest request = requestRepositoriy.getById(requestDto.getId());
-
+        CreditRequest request = requestRepository.getById(requestDto.getId());
+        if (request != null) {
             RequestStatus status = request.getStatus();
-
             request.setSum(requestDto.getSum());
             request.setPeriod(requestDto.getPeriod());
-            if (status == null) {
-                requestDto.setStatus(RequestStatus.WAITING);
-            }
+            request.setStatus(status);
             if (status != RequestStatus.WAITING) {
                 throw new IllegalArgumentException("Редактировать запрос со статусом: " + status + " запрещено");
             }
-            request.setStatus(status);
             //   request.setCreditResponse(responseRepository.findById(requestDto.getResponseId()).orElse(null));
             if (requestDto.getResponseId() != null) {
                 if (responseRepository.existsById(requestDto.getResponseId())) {
@@ -74,7 +69,7 @@ public class RequestService {
             } else {
                 request.setCreditResponse(null);
             }
-            requestRepositoriy.save(request);
+            requestRepository.save(request);
         } else {
             throw new NoSuchElementException();
         }
